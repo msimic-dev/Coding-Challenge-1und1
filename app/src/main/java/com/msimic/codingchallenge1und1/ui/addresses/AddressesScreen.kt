6e61fc24dc196d6a8ed17fa0e396d35d847b49d4
msimic <m.simic.dev@gmail.com>
2023-11-20 11:52:10 +0100

    Add SalesmanItem circle component

--- app/src/main/java/com/msimic/codingchallenge1und1/ui/addresses/AddressesScreen.kt
+++ app/src/main/java/com/msimic/codingchallenge1und1/ui/addresses/AddressesScreen.kt
@@ -1,10 +1,22 @@
 package com.msimic.codingchallenge1und1.ui.addresses
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun AddressesScreen(
     viewModel: AddressesViewModel
 ) {
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column {
 
+        }
+    }
 }
--- app/src/main/java/com/msimic/codingchallenge1und1/ui/addresses/SalesmanItem.kt
+++ app/src/main/java/com/msimic/codingchallenge1und1/ui/addresses/SalesmanItem.kt
@@ -1,15 +1,50 @@
 package com.msimic.codingchallenge1und1.ui.addresses
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.msimic.codingchallenge1und1.ui.theme.CodingChallenge1und1Theme
+import java.util.jar.Attributes.Name
 
 @Composable
 fun SalesmanItem(
     modifier: Modifier = Modifier,
+    isCollapsed: Boolean = false,
+    onClick: () -> Unit = {},
 ) {
 
+}
+
+@Composable
+fun NameFirstLetterIndicator(
+    letter: Char,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray, CircleShape)
+            .size(42.dp)
+            .clip(CircleShape)
+    ) {
+        Text(
+            text = letter.toString(),
+            modifier = Modifier.align(Alignment.Center),
+            fontSize = 32.sp,
+        )
+    }
 }
 
 @Preview
